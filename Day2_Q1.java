4f67ebacf81885004e5b787a7988efdf4602fc11
Maharshi Dindoliwala <maharshi1731@gmail.com>
2026-01-16 15:24:55 +0530

    day2

---
+++ Day2_Q1.java
@@ -0,0 +1,9 @@
+//WAP in java to print your name, prn, address in 3 different lines
+
+public class Day2_Q1 {
+    public static void main(){
+        System.out.println("Maharshi");
+        System.out.println("24*7*1**100");
+        System.out.println("127.0.0.1");
+    }    
+}
