336a4d4e06c06247f441bd643a38ab10755a0f1f
Maharshi Dindoliwala <maharshi1731@gmail.com>
2026-01-18 18:46:39 +0530

    day2_HW

--- Day2_Q2.java
+++ Day2_Q2.java
@@ -1,5 +1,11 @@
 //WAP in java to divide 2 numbers where you should be checking the condition of /0 error
-
 public class Day2_Q2 {
-    
+    public static void main(String[] args){
+        int a =21,b=21;
+        div(a,b);
+    }
+    public static void div(int a, int b){
+        if(a != 0) System.out.println("Division:"+(b/a));
+        else System.out.println("Invalid Input");
+    }
 }
