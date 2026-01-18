75343a3b1a434609fee4d573e9a7c8feed4ded3a
Maharshi Dindoliwala <maharshi1731@gmail.com>
2026-01-18 18:55:33 +0530

    day2_HW

--- Day2_Q3.java
+++ Day2_Q3.java
@@ -1,3 +1,11 @@
+//WAP in java where you should be passing 2 variables inside a function with float datatype and performing their addition inside the main function using double datatype.
+
 public class Day2_Q3 {
-    
+    public static void main(){
+        float a = 10.3f;
+        float b = 11.7f;
+        double sum = a+b;
+        System.out.println("Float Addition: "+sum);
+    }
 }
+    
